47d8ec7aeb1e3c172c0fada68ec6042a56305206
Peter Wendl <mail@peterwendl.de>
2024-12-23 18:47:00 +0100

    testing

--- tests/sample01.py
+++ tests/sample01.py
@@ -5,8 +5,16 @@ data["str_key"] = "hello"
 
 d = dicta.Dicta(data)
 d.bind_file("data.json")
-d["int_key"] = 9
-
+d["int_key"] = 0
 d.update(float_key=3.14,another_str_key="world!")
+d["dict_key"] = {"key1": "value1", "key2": "value2"}
+
+def callback():
+    print("bool_key changed to", d["bool_key"])
+
+d.bind_callback(callback)
+
+d["bool_key"] = False
+d["bool_key"] = True
 
 print(d.stringify())
