3050e49de6e00962d43a14e8ebecf0afe2eea6ad
Peter Wendl <mail@peterwendl.de>
2024-12-23 12:39:22 +0100

    bugfix

--- tests/sample01.py
+++ tests/sample01.py
@@ -4,9 +4,9 @@ data = {}
 data["str_key"] = "hello"
 
 d = dicta.Dicta(data)
-d.syncFile("data.json")
+d.bind_file("data.json")
 d["int_key"] = 9
 
-d.importData(float_key=3.14,another_str_key="world!")
+d.update(float_key=3.14,another_str_key="world!")
 
 print(d.stringify())
